6716cd649eb3c290c477358ce9d232983e560ec7
Jason Balinski <jabalins@microsoft.com>
2019-02-04 23:58:17 -0800

    updated css a bit

--- todo/src/store-types/StoreExtension.ts
+++ todo/src/store-types/StoreExtension.ts
@@ -1,3 +1,9 @@
+/**
+ * Abstract class used to create mutators, selectors, actions and viewSelectors.
+ * Keeps a private reference to the state so that it can me accessed by the methods
+ * defined on each of those components, but always keeps the state hidden from
+ * react components.
+ */
 export abstract class StoreExtension<T> {
     constructor(protected state:T) {}
 }
--- todo/src/store-types/store.ts
+++ todo/src/store-types/store.ts
@@ -1,11 +1,15 @@
 import {StoreExtension} from "./StoreExtension";
 
 /**
+ * Simple generic store class. Serves as a strongly typed
+ * public interface into the store, hiding actual state
+ * but publishing actions and viewSelectors.
+ *
  * Types:
  * S - state
  * A - actions
  * V - ViewSelectors
  */
 export class Store<S,A,V> {
-    constructor(protected state:S, public actions: A, public viewSelectors: V) {}
+    constructor(private state:S, public actions: A, public viewSelectors: V) {}
 }
